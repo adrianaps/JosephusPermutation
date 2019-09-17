8af0b2ab0b93cc70170513915f489e6843f51e61
adriana <adrianapenate98@gmail.com>
2019-09-17 09:53:45 +0100

    First iteration

---
+++ src/main/java/josephusPermutation/JosephusPermutation.java
@@ -0,0 +1,7 @@
+package josephusPermutation;
+
+public class JosephusPermutation {
+    public static int[] josephus(int[] soldiers, int killed) {
+        return new int[] {1};
+    }
+}
---
+++ src/test/java/josephusPermutation/JosephusPermutation_.java
@@ -0,0 +1,13 @@
+package josephusPermutation;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JosephusPermutation_ {
+    @Test
+    public void given_array_with_1_lenght_returns_same_array() {
+        assertThat(JosephusPermutation.josephus(new int[] {1}, 3)).isEqualTo(new int[] {1});
+    }
+
+}
