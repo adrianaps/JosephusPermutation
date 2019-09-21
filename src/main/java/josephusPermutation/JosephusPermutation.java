8ccaf5179f2227ba80001e7150eff3d93b0b9f7e
adriana <adrianapenate98@gmail.com>
2019-09-21 13:01:11 +0100

    Fifth iteration

--- src/main/java/josephusPermutation/JosephusPermutation.java
+++ src/main/java/josephusPermutation/JosephusPermutation.java
@@ -2,16 +2,11 @@ package josephusPermutation;
 
 public class JosephusPermutation {
     public static int[] josephus(int[] soldiers, int killed) {
-        if(soldiers.length <= 2 && killed % 2 != 0 || killed == 0){
-            return soldiers;
-        }else if (soldiers.length <= 2){
-            int aux = soldiers[0];
-            soldiers[0] = soldiers[1];
-            soldiers[1] = aux;
+        if(soldiers.length <= 2 && killed % 2 != 0 || killed <= 1){
             return soldiers;
         }
 
-        int aux=soldiers[2];
+        int aux=soldiers[killed-1];
         System.arraycopy(soldiers, 0, soldiers, 1, soldiers.length - 1);
         soldiers[0]=aux;
         return soldiers;
--- src/test/java/josephusPermutation/JosephusPermutation_.java
+++ src/test/java/josephusPermutation/JosephusPermutation_.java
@@ -34,4 +34,9 @@ public class JosephusPermutation_ {
     public void given_array_every_0_returns_same_array() {
         assertThat(JosephusPermutation.josephus(new int[] {1,2}, 0)).isEqualTo(new int[] {1,2});
     }
+
+    @Test
+    public void given_array_with_3_lenght_every_1_returns_same_array() {
+        assertThat(JosephusPermutation.josephus(new int[] {1,2,3}, 1)).isEqualTo(new int[] {1,2,3});
+    }
 }
