914c18072df73058954331a50d069772a9977e44
adriana <adrianapenate98@gmail.com>
2019-09-21 11:17:04 +0100

    Second iteration

--- src/main/java/josephusPermutation/JosephusPermutation.java
+++ src/main/java/josephusPermutation/JosephusPermutation.java
@@ -2,6 +2,6 @@ package josephusPermutation;
 
 public class JosephusPermutation {
     public static int[] josephus(int[] soldiers, int killed) {
-        return new int[] {1};
+        return soldiers;
     }
 }
--- src/test/java/josephusPermutation/JosephusPermutation_.java
+++ src/test/java/josephusPermutation/JosephusPermutation_.java
@@ -10,4 +10,9 @@ public class JosephusPermutation_ {
         assertThat(JosephusPermutation.josephus(new int[] {1}, 3)).isEqualTo(new int[] {1});
     }
 
+    @Test
+    public void given_array_with_0_lenght_returns_same_array() {
+        assertThat(JosephusPermutation.josephus(new int[] {}, 3)).isEqualTo(new int[] {});
+    }
+
 }
