714bc73f5edc3b9941ebdfa9941f809625509da8
adriana <adrianapenate98@gmail.com>
2019-09-21 11:39:28 +0100

    Third iteration

--- src/main/java/josephusPermutation/JosephusPermutation.java
+++ src/main/java/josephusPermutation/JosephusPermutation.java
@@ -2,6 +2,9 @@ package josephusPermutation;
 
 public class JosephusPermutation {
     public static int[] josephus(int[] soldiers, int killed) {
+        int aux=soldiers[2];
+        System.arraycopy(soldiers, 0, soldiers, 1, soldiers.length - 1);
+        soldiers[0]=aux;
         return soldiers;
     }
 }
--- src/test/java/josephusPermutation/JosephusPermutation_.java
+++ src/test/java/josephusPermutation/JosephusPermutation_.java
@@ -6,13 +6,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JosephusPermutation_ {
     @Test
-    public void given_array_with_1_lenght_returns_same_array() {
+    public void given_array_with_1_lenght_every_3_returns_same_array() {
         assertThat(JosephusPermutation.josephus(new int[] {1}, 3)).isEqualTo(new int[] {1});
     }
 
     @Test
-    public void given_array_with_0_lenght_returns_same_array() {
+    public void given_array_with_0_lenght_every_3_returns_same_array() {
         assertThat(JosephusPermutation.josephus(new int[] {}, 3)).isEqualTo(new int[] {});
     }
+
+    @Test
+    public void given_array_with_2_lenght_every_3_returns_same_array() {
+        assertThat(JosephusPermutation.josephus(new int[] {1,2}, 3)).isEqualTo(new int[] {1,2});
+    }
+
+    @Test
+    public void given_array_with_3_lenght_every_3_returns_reordenated_array() {
+        assertThat(JosephusPermutation.josephus(new int[] {1,2,3}, 3)).isEqualTo(new int[] {3,1,2});
+    }
+
+
 
 }
