d6b39b3cf93277f02ed8b8d3d31df74a1bb02028
adriana <adrianapenate98@gmail.com>
2019-09-25 18:15:56 +0100

    Second iteration test

--- src/test/java/josephusPermutation/JosephusPermutation_.java
+++ src/test/java/josephusPermutation/JosephusPermutation_.java
@@ -7,12 +7,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JosephusPermutation_ {
     @Test
-    public void given_empty_array_with_0_killed_should_give_empty_array() {
+    public void given_array_with_1_or_less_killed_should_give_same_array() {
         assertThat(josephus(new int[] {}, 0)).isEqualTo(new int[]{});
+        assertThat(josephus(new int[] {1}, 0)).isEqualTo(new int[]{1});
+        assertThat(josephus(new int[] {1}, -1)).isEqualTo(new int[]{1});
+        assertThat(josephus(new int[] {1}, 1)).isEqualTo(new int[]{1});
+        assertThat(josephus(new int[] {1, 2}, 1)).isEqualTo(new int[]{1, 2});
     }
 
     @Test
-    public void given_array_with_0_killed_should_give_same_array() {
-        assertThat(josephus(new int[] {1}, 0)).isEqualTo(new int[]{1});
+    public void given_array_with_1_length_and_2_or_less_killed_should_give_same_array() {
+        assertThat(josephus(new int[] {1}, 2)).isEqualTo(new int[]{1});
+        assertThat(josephus(new int[] {1}, 5)).isEqualTo(new int[]{1});
+    }
+
+    @Test
+    public void given_array_with_2_length_and_2_killed_should_give_same_array() {
+        assertThat(josephus(new int[] {1, 2}, 2)).isEqualTo(new int[]{2, 1});
     }
 }
