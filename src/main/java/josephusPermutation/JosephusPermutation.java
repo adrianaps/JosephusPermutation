1ed5c82fcedd520db6f51e03a563ec9e8f92bbbd
adriana <adrianapenate98@gmail.com>
2019-09-25 17:51:58 +0100

    Restarting - First iteration

--- src/main/java/josephusPermutation/JosephusPermutation.java
+++ src/main/java/josephusPermutation/JosephusPermutation.java
@@ -3,24 +3,6 @@ package josephusPermutation;
 public class JosephusPermutation {
 
     public static int[] josephus(int[] soldiers, int killed) {
-        if(soldiers.length == 2 && killed % 2 != 0 || killed <= 1 || soldiers.length < 2) return soldiers;
-
-        if(soldiers.length <= 3) return permutateKilledSoldier(soldiers, killed-1, 0);
-
-        int lastKilled=-1;
-        for(int i=0; i<soldiers.length-1; i++){
-            if((killed%(soldiers.length-i))+lastKilled <i) lastKilled+=i;
-            soldiers = permutateKilledSoldier(soldiers,(killed%(soldiers.length-i))+lastKilled, i);
-            lastKilled = killed%(soldiers.length-i)+lastKilled;
-        }
         return soldiers;
     }
-
-    private static int[] permutateKilledSoldier(int[] soldiers, int killedSoldier, int liveSoldier) {
-        int deadSoldier=soldiers[killedSoldier];
-        System.arraycopy(soldiers, liveSoldier, soldiers, liveSoldier+1, killedSoldier-liveSoldier);
-        soldiers[liveSoldier]=deadSoldier;
-        return soldiers;
-    }
-
 }
--- src/test/java/josephusPermutation/JosephusPermutation_.java
+++ src/test/java/josephusPermutation/JosephusPermutation_.java
@@ -2,58 +2,17 @@ package josephusPermutation;
 
 import org.junit.Test;
 
+import static josephusPermutation.JosephusPermutation.josephus;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JosephusPermutation_ {
     @Test
-    public void given_array_with_less_than_2_lenght_returns_same_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1}, 3)).isEqualTo(new int[] {1});
-        assertThat(JosephusPermutation.josephus(new int[] {}, 3)).isEqualTo(new int[] {});
+    public void given_empty_array_with_0_killed_should_give_empty_array() {
+        assertThat(josephus(new int[] {}, 0)).isEqualTo(new int[]{});
     }
 
     @Test
-    public void given_array_every_1_or_less_returns_same_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1,2}, 0)).isEqualTo(new int[] {1,2});
-        assertThat(JosephusPermutation.josephus(new int[] {1,2,3}, 1)).isEqualTo(new int[] {1,2,3});
-    }
-
-    @Test
-    public void given_array_with_2_lenght_3_every__returns_same_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1,2}, 3)).isEqualTo(new int[] {1,2});
-    }
-
-    @Test
-    public void given_array_with_2_lenght_every_2_returns_permutated_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1,2}, 2)).isEqualTo(new int[] {2,1});
-    }
-
-    @Test
-    public void given_array_with_3_lenght_every_3_returns_permutated_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1,2,3}, 3)).isEqualTo(new int[] {3,1,2});
-    }
-
-    @Test
-    public void given_array_with_3_lenght_every_2_returns_permutated_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1,2,3}, 2)).isEqualTo(new int[] {2,1,3});
-    }
-
-    @Test
-    public void given_array_with_4_lenght_every_2_returns_permutated_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1,2,3,4}, 2)).isEqualTo(new int[] {2,4,3,1});
-    }
-
-    @Test
-    public void given_array_with_2_lenght_every_5_returns_same_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1,2}, 5)).isEqualTo(new int[] {1,2});
-    }
-
-    @Test
-    public void given_array_with_4_lenght_every_3_returns_permutated_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1,2,3,4}, 3)).isEqualTo(new int[] {3,2,4,1});
-    }
-
-    @Test
-    public void given_array_with_4_lenght_every_6_returns_permutated_array() {
-        assertThat(JosephusPermutation.josephus(new int[] {1,2,3,4}, 6)).isEqualTo(new int[] {2,1,4,3});
+    public void given_array_with_0_killed_should_give_same_array() {
+        assertThat(josephus(new int[] {1}, 0)).isEqualTo(new int[]{1});
     }
 }
