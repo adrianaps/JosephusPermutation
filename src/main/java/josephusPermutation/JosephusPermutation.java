9e919eaaa4c509a4fb7f2fc629675459d82b3952
adriana <adrianapenate98@gmail.com>
2019-09-25 20:03:42 +0100

    Third iteration

--- src/main/java/josephusPermutation/JosephusPermutation.java
+++ src/main/java/josephusPermutation/JosephusPermutation.java
@@ -1,8 +1,41 @@
 package josephusPermutation;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class JosephusPermutation {
 
     public static int[] josephus(int[] soldiers, int killed) {
-        return soldiers;
+        if(killed <= 1 || soldiers.length==1) return soldiers;
+
+        return getKilledSoldiers(Arrays.stream(soldiers).boxed().collect(Collectors.toList()), killed);
+    }
+
+    private static int[] getKilledSoldiers(List<Integer> soldiers, int killed) {
+        int[] deadSoldiers = new int[soldiers.size()];
+        for (int i = 1, alreadyDead = 0 , count = 1; i <= soldiers.size() ; i++, count++) {
+            if(count % killed == 0) {
+                deadSoldiers[alreadyDead] = soldiers.remove(i-1);
+                count=0;
+                alreadyDead++;
+                i--;
+            }
+            if(oneSoldierLeft(deadSoldiers.length, alreadyDead)){
+                deadSoldiers[alreadyDead] = soldiers.remove(0);
+                break;
+            }
+            if(areSoldiersStillAlive(soldiers.size(), deadSoldiers.length-1, i, alreadyDead)) i=0;
+
+        }
+        return deadSoldiers;
+    }
+
+    private static boolean oneSoldierLeft(int deadSoldiers, int alreadyDead) {
+        return alreadyDead == deadSoldiers - 1;
+    }
+
+    private static boolean areSoldiersStillAlive(int soldiersAlive, int deadSoldiers, int i, int pos) {
+        return pos != deadSoldiers && i == soldiersAlive;
     }
 }
--- src/test/java/josephusPermutation/JosephusPermutation_.java
+++ src/test/java/josephusPermutation/JosephusPermutation_.java
@@ -7,12 +7,49 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JosephusPermutation_ {
     @Test
-    public void given_empty_array_with_0_killed_should_give_empty_array() {
+    public void given_array_with_1_or_less_killed_should_give_same_array() {
         assertThat(josephus(new int[] {}, 0)).isEqualTo(new int[]{});
+        assertThat(josephus(new int[] {1}, 0)).isEqualTo(new int[]{1});
+        assertThat(josephus(new int[] {1}, -1)).isEqualTo(new int[]{1});
+        assertThat(josephus(new int[] {1}, 1)).isEqualTo(new int[]{1});
+        assertThat(josephus(new int[] {1, 2}, 1)).isEqualTo(new int[]{1, 2});
     }
 
     @Test
-    public void given_array_with_0_killed_should_give_same_array() {
-        assertThat(josephus(new int[] {1}, 0)).isEqualTo(new int[]{1});
+    public void given_array_with_1_length_and_2_or_less_killed_should_give_same_array() {
+        assertThat(josephus(new int[] {1}, 2)).isEqualTo(new int[]{1});
+        assertThat(josephus(new int[] {1}, 5)).isEqualTo(new int[]{1});
+    }
+
+    @Test
+    public void given_array_with_2_length_and_even_killed_should_give_permutated_array() {
+        assertThat(josephus(new int[] {1, 2}, 2)).isEqualTo(new int[]{2, 1});
+        assertThat(josephus(new int[] {1, 2}, 4)).isEqualTo(new int[]{2, 1});
+        assertThat(josephus(new int[] {1, 2}, 6)).isEqualTo(new int[]{2, 1});
+        assertThat(josephus(new int[] {1, 2}, 12)).isEqualTo(new int[]{2, 1});
+    }
+
+    @Test
+    public void given_array_with_2_length_and_odd_killed_should_give_same_array() {
+        assertThat(josephus(new int[] {1, 2}, 3)).isEqualTo(new int[]{1, 2});
+        assertThat(josephus(new int[] {1, 2}, 5)).isEqualTo(new int[]{1, 2});
+        assertThat(josephus(new int[] {1, 2}, 7)).isEqualTo(new int[]{1, 2});
+        assertThat(josephus(new int[] {1, 2}, 21)).isEqualTo(new int[]{1, 2});
+    }
+
+    @Test
+    public void given_array_with_3_length_and_even_killed_should_give_same_array() {
+        assertThat(josephus(new int[] {1, 2, 3}, 2)).isEqualTo(new int[]{2, 1, 3});
+        assertThat(josephus(new int[] {1, 2, 3}, 4)).isEqualTo(new int[]{1, 3, 2});
+        assertThat(josephus(new int[] {1, 2, 3}, 6)).isEqualTo(new int[]{3,2,1});
+        assertThat(josephus(new int[] {1, 2, 3}, 8)).isEqualTo(new int[]{2, 1, 3});
+        assertThat(josephus(new int[] {1, 2, 3}, 10)).isEqualTo(new int[]{1, 3, 2});
+    }
+
+    @Test
+    public void given_array_with_3_length_and_odd_killed_should_give_same_array() {
+        assertThat(josephus(new int[] {1, 2, 3}, 3)).isEqualTo(new int[]{3,1,2});
+        assertThat(josephus(new int[] {1, 2, 3}, 5)).isEqualTo(new int[]{2, 3, 1});
+        assertThat(josephus(new int[] {1, 2, 3}, 7)).isEqualTo(new int[]{1, 2, 3});
     }
 }
