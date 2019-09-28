91aa89930b7a2c42d67831a06d7cd083bae6e1b9
adriana <adrianapenate98@gmail.com>
2019-09-28 12:29:26 +0100

    Fourth iteration

--- src/test/java/josephusPermutation/JosephusPermutation_.java
+++ src/test/java/josephusPermutation/JosephusPermutation_.java
@@ -52,4 +52,20 @@ public class JosephusPermutation_ {
         assertThat(josephus(new int[] {1, 2, 3}, 5)).isEqualTo(new int[]{2, 3, 1});
         assertThat(josephus(new int[] {1, 2, 3}, 7)).isEqualTo(new int[]{1, 2, 3});
     }
+
+    @Test
+    public void given_array_with_4_length_and_odd_killed_should_give_same_array() {
+        assertThat(josephus(new int[] {1, 2, 3, 4}, 3)).isEqualTo(new int[]{3,2, 4, 1});
+        assertThat(josephus(new int[] {1, 2, 3, 4}, 5)).isEqualTo(new int[]{1, 3, 4, 2});
+        assertThat(josephus(new int[] {1, 2, 3, 4}, 7)).isEqualTo(new int[]{3, 4, 1, 2});
+    }
+
+    @Test
+    public void given_array_with_5_length_and_even_killed_should_give_same_array() {
+        assertThat(josephus(new int[]{1, 2, 3, 4, 5}, 2)).isEqualTo(new int[]{2, 4, 1, 5, 3});
+        assertThat(josephus(new int[]{1, 2, 3, 4, 5}, 4)).isEqualTo(new int[]{4, 3, 5, 2, 1});
+        assertThat(josephus(new int[]{1, 2, 3, 4, 5}, 6)).isEqualTo(new int[]{1, 3, 2, 5, 4});
+        assertThat(josephus(new int[]{1, 2, 3, 4, 5}, 8)).isEqualTo(new int[]{3, 2, 5, 4, 1});
+        assertThat(josephus(new int[]{1, 2, 3, 4, 5}, 10)).isEqualTo(new int[]{5, 2, 3, 1, 4});
+    }
 }
